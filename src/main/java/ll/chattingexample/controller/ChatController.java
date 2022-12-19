6a956569537b4fb359d7fe430ea600eee2a0e0a3
cmkxak <gcm466@naver.com>
2022-12-19 12:53:14 +0900

    add : 메시지 작성, 조회 API 구현

--- src/main/java/ll/chattingexample/controller/ChatController.java
+++ src/main/java/ll/chattingexample/controller/ChatController.java
@@ -1,17 +1,34 @@
 package ll.chattingexample.controller;
 
+import ll.chattingexample.domain.ChatMessage;
+import ll.chattingexample.domain.dto.*;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @RequestMapping("/chat")
 public class ChatController {
 
+    private List<ChatMessage> chatMessages = new ArrayList<>();
+
     @PostMapping("/writeMessage")
     @ResponseBody
-    public String chat(){
-        return "메시지가 작성되었습니다.";
+    public RsData<WriteMessageResponse> writeMessage(@RequestBody WriteMessageRequest request){
+        ChatMessage message = new ChatMessage(request.getAuthorName(), request.getContent());
+
+        chatMessages.add(message);
+
+        return new RsData<>("S-1", "메시지가 작성되었습니다.", new WriteMessageResponse(message.getId()));
+    }
+
+    @GetMapping("/messages")
+    @ResponseBody
+    public RsData<MessageResponse> getMessages(MessageRequest request){
+        List<ChatMessage> messages = chatMessages;
+
+        return new RsData<>("S-1", "성공", new MessageResponse(messages, messages.size()));
     }
 }
