b23aa887b90cdd7b6942482de5586387bbfc2b3b
cmkxak <gcm466@naver.com>
2022-12-19 23:12:24 +0900

    add : message filter

--- src/main/java/ll/chattingexample/controller/ChatController.java
+++ src/main/java/ll/chattingexample/controller/ChatController.java
@@ -2,16 +2,20 @@ package ll.chattingexample.controller;
 
 import ll.chattingexample.domain.ChatMessage;
 import ll.chattingexample.domain.dto.*;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.IntStream;
 
-@Controller
+@RequiredArgsConstructor
 @RequestMapping("/chat")
+@Controller
 public class ChatController {
-
     private List<ChatMessage> chatMessages = new ArrayList<>();
 
     @PostMapping("/writeMessage")
@@ -27,8 +31,23 @@ public class ChatController {
     @GetMapping("/messages")
     @ResponseBody
     public RsData<MessageResponse> getMessages(MessageRequest request){
+        List<ChatMessage> messages = filterMessage(request);
+        return new RsData<>("S-1", "성공", new MessageResponse(messages, messages.size()));
+    }
+
+    private List<ChatMessage> filterMessage(MessageRequest request) {
         List<ChatMessage> messages = chatMessages;
 
-        return new RsData<>("S-1", "성공", new MessageResponse(messages, messages.size()));
+        if (request.getFromId() != null){
+            int idx = IntStream.range(0, messages.size())
+                    .filter(i -> chatMessages.get(i).getId() == request.getFromId())
+                    .findFirst()
+                    .orElse(-1);
+
+            if (idx != -1){
+                messages = messages.subList(idx, messages.size());
+            }
+        }
+        return messages;
     }
 }
