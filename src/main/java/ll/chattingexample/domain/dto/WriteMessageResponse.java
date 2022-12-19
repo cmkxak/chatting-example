17dd8a904861a71004e19a78805aca7a33587120
cmkxak <gcm466@naver.com>
2022-12-19 12:51:43 +0900

    add : message 작성 시, 요청,응답 DTO

---
+++ src/main/java/ll/chattingexample/domain/dto/WriteMessageResponse.java
@@ -0,0 +1,10 @@
+package ll.chattingexample.domain.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class WriteMessageResponse {
+    private Long id;
+}
